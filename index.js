f1b045b042880f95317140c456987f34ef65a5d4
konok101 <rockykonok@gmail.com>
2022-09-05 21:49:16 +0600

    database connected with node

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cli = require('nodemon/lib/cli');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8gvjjte.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('db connected');
+    }
+    finally{
+        
+    }
+
+}
+run().catch(console.dir);
+
+console.log(uri)
 app.get('/', (req, res) => {
   res.send('Hello Doctors!')
 })
