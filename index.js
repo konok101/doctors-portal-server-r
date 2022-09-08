f0f75ffb25f955847c67597e8de5e7a738725c77
konok101 <rockykonok@gmail.com>
2022-09-09 00:48:50 +0600

    booking information post part done and same information not allow ok

--- index.js
+++ index.js
@@ -14,41 +14,46 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
-        await client.connect();
-         const serviceCollection = client.db('doctors_portal').collection('services');
-         const  bookingCollection = client.db('doctors_portal').collection('bookings');
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db('doctors_portal').collection('services');
+    const bookingCollection = client.db('doctors_portal').collection('bookings');
 
-         app.get('/service', async(req, res)=>{
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
+    app.get('/service', async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
 
-         });
+    });
 
 
-         /**
-          * API Naming convention
-          * app.get('/booking') //get all booking in this collection
-          * app.get('/booking/:id') // get a specific booking
-          * app.post('/booking') //add a new booking
-          * app.patch('/booking/:id') // update booking signle person information
-          * app.delete('/booking/:id')//for delete
-          * 
-          * 
-         */
+    /**
+     * API Naming convention
+     * app.get('/booking') //get all booking in this collection
+     * app.get('/booking/:id') // get a specific booking
+     * app.post('/booking') //add a new booking
+     * app.patch('/booking/:id') // update booking signle person information
+     * app.delete('/booking/:id')//for delete
+     * 
+     * 
+    */
 
-         app.post('/booking', async(req, res)=>{
-          const booking= req.body; //data read
-          const result= await bookingCollection.insertOne(booking);
-          res.send(result);
-         })
-    }
-    finally{
+    app.post('/booking', async (req, res) => {
+      const booking = req.body;
+      const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient }
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists })
+      }
+      const result = await bookingCollection.insertOne(booking);
+      res.send({ success: true, result });
+    })
+  }
+  finally {
 
-    }
+  }
 
 }
 run().catch(console.dir);
